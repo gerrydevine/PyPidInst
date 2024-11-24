f970717fd01e95c0368eb745662ab519922db757
Gerry Devine <gerry.devine@unsw.edu.au>
2024-11-25 08:43:58 +1100

    Updated install_requires

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='PyPIDInst',
     version='1.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires = [
+        'requests',
+        'python-dotenv'
+    ]
 )
